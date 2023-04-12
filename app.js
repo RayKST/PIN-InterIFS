d907f6b43e5650a8f3bdd8ac6f862a1ec8ac2131
Kauan <jailma.kmp@gmail.com>
2023-04-12 01:41:07 -0300

    [feat] render new views at the endpoints

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const ejs = require('ejs')
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('sample.db');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('bem vindo a tela principal');
+  res.send('Tela principal')
 });
 
 app.get('/login', (req, res) => {
-    res.send('Tela de login');
+  ejs.renderFile('templates/login.ejs', (err, html) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    } else {
+      res.send(html);
+    }
+  });
 });
 
 app.get('/recuperar-senha', (req, res) => {
@@ -14,9 +27,16 @@ app.get('/recuperar-senha', (req, res) => {
 });
 
 app.get('/criar-usuario', (req, res) => {
-    res.send('Tela de criar usuario');
+  ejs.renderFile('templates/create-account.ejs', (err, html) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    } else {
+      res.send(html);
+    }
+  });
 });
 
-app.listen(3000, () => {
+app.listen(3000, () => {    
   console.log('Server rodando na porta 3000 pae');
 });
